82016b77c8d29896682b7e017b9380da9ef06f13
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-05-09 11:58:12 +0200

    Add the class attribute to the /source element

--- WebDriverAgentLib/Utilities/FBXPath.m
+++ WebDriverAgentLib/Utilities/FBXPath.m
@@ -93,6 +93,10 @@
 
 #endif
 
+@interface FBClassAttribute : FBDimensionAttribute
+
+@end
+
 const static char *_UTF8Encoding = "UTF-8";
 
 static NSString *const kXMLIndexPathKey = @"private_indexPath";
@@ -498,7 +502,8 @@ static NSString *const FBAbstractMethodInvocationException = @"AbstractMethodInv
            FBXAttribute.class,
            FBYAttribute.class,
            FBWidthAttribute.class,
-           FBHeightAttribute.class];
+           FBHeightAttribute.class,
+           FBClassAttribute.class];
 }
 
 @end
@@ -735,4 +740,18 @@ static NSString *const FBAbstractMethodInvocationException = @"AbstractMethodInv
   }
   return rc;
 }
+@end
+
+@implementation FBClassAttribute
+
++ (NSString *)name
+{
+  return @"class";
+}
+
++ (NSString *)valueForElement:(id<FBElement>)element
+{
+  return element.wdClass;
+}
+
 @end
