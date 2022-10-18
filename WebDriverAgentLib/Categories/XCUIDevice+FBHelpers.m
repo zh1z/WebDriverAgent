1fdd515638195737db7f25de2d76a4aab4db5382
zh1z <zh1z@live.cn>
2022-10-18 17:29:14 +0800

    update scroll & home

--- WebDriverAgentLib/Categories/XCUIDevice+FBHelpers.m
+++ WebDriverAgentLib/Categories/XCUIDevice+FBHelpers.m
@@ -55,7 +55,9 @@ static bool fb_isLocked;
 
 - (BOOL)fb_goToHomescreenWithError:(NSError **)error
 {
-  return [FBApplication fb_switchToSystemApplicationWithError:error];
+//  return [FBApplication fb_switchToSystemApplicationWithError:error];
+  [self pressButton:XCUIDeviceButtonHome];
+  return YES;
 }
 
 - (BOOL)fb_lockScreen:(NSError **)error
